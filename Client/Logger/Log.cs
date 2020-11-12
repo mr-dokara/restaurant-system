6e1860bf2b657e6f9717b13e020cb22576825619
Semyon Samarin <loner40466@gmail.com>
2020-11-12 16:22:44 +0300

    * Created logger class for logging ours project.

---
+++ Client/Logger/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Logger
+{
+    /// <summary>
+    /// Статический класс для логирования.
+    /// </summary>
+    public static class Log
+    {
+        private static string _fileName;
+
+        /// <summary>
+        /// Возвращает и задает имя файла.
+        /// <para><b>Расширение задавать не нужно.</b></para>
+        /// </summary>
+        public static string FileName
+        {
+            get => _fileName;
+            set
+            {
+                var fileNameChecker = new Regex(@"^([A-Za-z0-9_\.]+)$");
+
+                if (fileNameChecker.IsMatch(value))
+                    _fileName = value + ".log";
+                else
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
